8c9732119c3b6b1fcaae3291dc46920442329cfa
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-10-22 16:41:35 -0400

    Added the PointGravity script

---
+++ Assets/script/PointGravity.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PointGravity : MonoBehaviour {
+	[Tooltip("The point in the world this object will gravitate towards")]
+	public Vector3 Center;
+
+	private float _gravity;
+
+	void Start() {
+		_gravity = Physics.gravity.magnitude;
+	}
+
+	void FixedUpdate() {
+		this.rigidbody.AddForce((this.Center - this.transform.position).normalized * _gravity * this.rigidbody.mass);
+	}
+}
