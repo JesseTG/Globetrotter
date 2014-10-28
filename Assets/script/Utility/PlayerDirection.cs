e79917a332d03558f35f16059460d75eaf7d3d5f
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-10-27 23:31:16 -0400

    Add a PlayerDirection enum

---
+++ Assets/script/Utility/PlayerDirection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PlayerDirection
+{
+    North,
+    South,
+    East,
+    West
+}
+
+public static class PlayerDirectionExtensions
+{
+    public static Vector3 DirectionVector (this PlayerDirection direction, Transform transform)
+    {
+        switch (direction) {
+        case PlayerDirection.North:
+            return transform.forward;
+        case PlayerDirection.South:
+            return transform.forward * -1;
+        case PlayerDirection.East:
+            return transform.right;
+        case PlayerDirection.West:
+            return transform.right * -1;
+        default:
+            return transform.forward;
+        }
+    }
+}
