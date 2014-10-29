97d635c3b9a2ab6f5bdb14782a9ecf6689f1a90d
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-10-29 15:08:46 -0400

    Move scripts to script folder

---
+++ Assets/script/PlayerMotion.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotion : MonoBehaviour
+{
+
+    public float Speed = 1;
+    private PlayerDirection _direction;
+
+    public PlayerDirection Direction {
+        get {
+            return _direction;
+        }
+
+        set {
+            this._direction = value;
+            if (value == PlayerDirection.North || value == PlayerDirection.South) {
+                this.rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation;
+            } else if (value == PlayerDirection.East || value == PlayerDirection.West) {
+                this.rigidbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
+            }
+        }
+    }
+
+    void Awake ()
+    {
+        this.Direction = PlayerDirection.North;
+    }
+
+    void FixedUpdate ()
+    {
+        this.transform.up = this.transform.position.normalized;
+
+        Vector3 r = Vector3.Cross (Vector3.zero, this.transform.position);
+//        this.rigidbody.position += this.Direction.DirectionVector (this.transform) * Speed * Time.deltaTime;
+        this.rigidbody.velocity = this.Direction.DirectionVector (this.transform) * Speed;
+    }
+
+    // For player travelling up walls: Slant the walls towards the player slightly (or at least the colliders)
+    // Also, try putting the beginning of the capsule slope right at the surface
+}
