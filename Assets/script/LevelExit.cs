a98bcfbd19de862fce9aadf5d13a3cdd598121e8
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-11-03 20:38:12 -0500

    Added the LevelExit component
    - Basic; just switches scenes, no exit animation yet

---
+++ Assets/script/LevelExit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class LevelExit : MonoBehaviour {
+    public string NextLevel;
+    public string PlayerTag = "Player";
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == PlayerTag) {
+            Application.LoadLevel (NextLevel);
+        }
+    }
+}
