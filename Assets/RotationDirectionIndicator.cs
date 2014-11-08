c00315416ab74414ac05dd6a8f645a7c71af7a6c
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-11-07 19:03:25 -0500

    Added the RotationDirectionIndicator

---
+++ Assets/RotationDirectionIndicator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+[RequireComponent(typeof(Graphic))]
+public class RotationDirectionIndicator : MonoBehaviour
+{
+    public RotationMode RotationMode;
+    [Tooltip("The material used to render this graphics when its rotation mode is being used")]
+    public Material
+        Selected;
+    public Material NotSelected;
+    private Image _image;
+
+    void Awake ()
+    {
+        this._image = this.GetComponent<Image> ();
+    }
+
+    public void OnRotationModeChanged (RotationMode old, RotationMode current)
+    {
+        this._image.material = (current == this.RotationMode) ? this.Selected : this.NotSelected;
+    }
+}
