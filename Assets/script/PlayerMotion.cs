66bece85082f8415145d1fe729eeecf208796f23
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-12-15 22:43:33 -0500

    Etc.

--- Assets/script/PlayerMotion.cs
+++ Assets/script/PlayerMotion.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.Events;
+using System;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMotion : MonoBehaviour
 {
     public float Speed = 1;
+    public float Acceleration = 20;
     public string WallTag = "Wall";
+    public PlayerDirection InitialDirection = PlayerDirection.North;
     private PlayerDirection _direction;
 
     public PlayerDirection Direction {
@@ -25,14 +28,14 @@ public class PlayerMotion : MonoBehaviour
 
     void Awake ()
     {
-        this.Direction = PlayerDirection.North;
+        this.Direction = this.InitialDirection;
     }
-
+    
     void FixedUpdate ()
     {
         this.transform.up = this.transform.position.normalized;
-
-        this.rigidbody.velocity = this.Direction.DirectionVector (this.transform) * Speed;
+        this.rigidbody.AddForce(this.transform.forward * this.Acceleration * this.rigidbody.mass);
+        this.rigidbody.velocity = Vector3.ClampMagnitude(this.rigidbody.velocity, Speed);
         // TODO: Make this play nicely with gravity
     }
 
@@ -43,6 +46,11 @@ public class PlayerMotion : MonoBehaviour
         }
     }
 
+    public void ChangeDirection(PlayerDirection old, PlayerDirection new_) {
+        this.Direction = new_;
+    }
+   
+
     // For player travelling up walls: Slant the walls towards the player slightly (or at least the colliders)
     // Also, try putting the beginning of the capsule slope right at the surface
 
@@ -50,3 +58,5 @@ public class PlayerMotion : MonoBehaviour
 		Speed = 0;
 	}
 }
+
+
