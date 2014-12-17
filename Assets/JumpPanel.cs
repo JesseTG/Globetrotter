99d4d21b78e6413cb33b6e9e9aabfc8f26a64320
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-12-16 21:55:19 -0500

    Added the Spring prefab (and accompanying JumpPanel script)
    - Plus all materials, models, textures, etc.

---
+++ Assets/JumpPanel.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpPanel : MonoBehaviour
+{
+    public float JumpForce = 10;
+    public string PlayerTag = "Player";
+
+    // Use this for initialization
+    void Start ()
+    {
+    
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+    
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == PlayerTag) {
+#if UNITY_EDITOR
+            Debug.Log ("Boing!", this);
+#endif
+            other.rigidbody.AddRelativeForce (0, this.JumpForce, 0, ForceMode.Impulse);
+        }
+    }
+}
