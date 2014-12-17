17c2fd9a0c6d72169941448038cff104131fb737
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-12-16 21:57:30 -0500

    Included Change Direction materials/images
    - Blank at the moment
    - Untested

---
+++ Assets/ChangeDirectionPanel.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+public class ChangeDirectionPanel : MonoBehaviour
+{
+
+    [Tooltip("The direction the player should start walking in (regardless of which way he is already)")]
+    public PlayerDirection
+        Direction = PlayerDirection.North;
+    public string PlayerTag = "Player";
+    public ChangeDirectionEvent OnWalkedOnChangeDirectionPanel;
+
+    void Start ()
+    {
+    
+    }
+    
+    // Update is called once per frame
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == PlayerTag) {
+            PlayerMotion motion = other.gameObject.GetComponent<PlayerMotion> ();
+
+            if (motion.Direction != this.Direction) {
+                this.OnWalkedOnChangeDirectionPanel.Invoke (motion.Direction, this.Direction);
+            }
+        }
+    }
+
+}
+
+[Serializable]
+public class ChangeDirectionEvent : UnityEvent<PlayerDirection, PlayerDirection>
+{
+}
